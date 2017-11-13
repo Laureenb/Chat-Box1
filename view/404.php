7e54441da7e0fa61af312c8b85aae828eca60af3
Laureenb <laureen_bataille@hotmail.com>
2017-11-13 14:45:50 +0100

    css v0.0.4

--- view/404.php
+++ view/404.php
@@ -1,1 +1,15 @@
-<p>je suis  404</p>
+<link rel="stylesheet" href="css/style.css">
+
+<body>
+	<section id="main_page">
+
+		<div id="page">
+			<h1>PAGE 404</h1>
+		</div>
+		<div id="text">
+			<h2>Désolé cette page n'existe plus...</h2>
+		</div>
+		<button class="bouton">REDIRECTION</a></button> 
+	
+	</section>
+</body>
