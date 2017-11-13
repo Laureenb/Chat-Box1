08220b05fe3a5f4a2b83d6a7e4dc5a4aeecfe8db
Slylnss <s.alonsious@web-siouss.be>
2017-11-13 14:59:42 +0100

    change connexion

--- view/connexion.php
+++ view/connexion.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-  <form action="../model/log-in.php" method="post">
+  <form action="model/log-in.php" method="post">
     <label for="utilisateur">Nom d'utilisateur</label>:<input type="text" name="utilisateur" value=""></br>
     <label for="mdp">Mot de passe</label>:<input type="password" name="mdp" value=""></br>
     <input type="submit" name="Login" value="Login">
