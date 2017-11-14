cd43466cb0b57074ef663cdf32b5849666077ca2
Unknown <thomaspoint@gmail.com>
2017-11-14 10:25:18 +0100

    login

--- model/log-in.php
+++ model/log-in.php
@@ -1,38 +1,18 @@
 <?php
 
-		// include('home.php');
-		// $req = $pdo->prepare("INSERT INTO utilisateurs (nom,motDePasse) VALUES (?,?)");
-		// $req->execute(['nom','mdp']);
+include('../model/home.php');
 
+$utilisateur = $_POST['utilisateur'];
+$password = $_POST['motDePasse'];
 
-
-		if(!empty($_POST['utilisateurs'])&& !empty($_POST['motDePasse']))
-		{
-			$utilisateurs=$_POST['utilisateurs'];
-			$mdp=$_POST['motDePasse'];
-			$mdp=md5($mdp);
-			$requete="SELECT nom, motDePasse FROM chatons WHERE nom = :utilisateurs AND motDePasse = :mdp";
-			$pdo->query("INSERT INTO utilisateurs ('nom','motDePasse') VALUES ('$utilisateurs','$motDePasse')");
-			include('home.php');
-			$connexion=connex();
-			$connex=$connexion->prepare($requete);
-			$connex->execute(array('utilisateurs' => $utilisateurs, 'motDePasse' => $mdp));
-			if($connex)
-				$ligne=$connex->fetch(PDO::FETCH_ASSOC);
-				$connex=closeCursor();
-				if($ligne){
-					$page = $ligne['page'];
-					header("location:".$page.".php");
-				}
-				else
-					if(!isset($ligne['utilisateurs']) || !isset($ligne["motDePasse"]) || empty($ligne['utilisateurs']) || empty($ligne['motDePasse']))
-					{
-						echo" Erreur nom utilisateur ou mot de passe";
-					}
-		}
-		else{
-			
-
-		var_dump($_POST);
+if(isset($_POST['utilisateur']),isset($_POST['motDePasse'])){
+	$query = $pdo->query("SELECT nom , motDePasse FROM utilisateurs WHERE nom='$utilisateur' AND motDePasse='$password'");
+	 if($query->rowCount() = 1){
+		 echo 'Vous etes mtn log!';
+	 } else {
+		 header('../view/inscription.php');
+	 }
 }
-?>
+
+
+ ?>
